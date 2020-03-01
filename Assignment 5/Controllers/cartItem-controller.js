9eb65bb6885b0337394cdc8a2778ed41944e656f
SamuelDowney <43819573+SamuelDowney@users.noreply.github.com>
2020-03-01 15:19:14 -0600

    Add files via upload

---
+++ Assignment 5/Controllers/cartItem-controller.js
@@ -0,0 +1,58 @@
+const {
+    getAllCartItems,
+    getCartItemsByCartItemId,
+    getCartItemsByItemId
+} = require('../services/cartItem-service');
+const {getCartItemsByCartId} = require('../services/cartItem-service');
+
+const getCartItemsRoute = (server) => {
+    server.route({
+        path: '/cart-items',
+        method: 'GET',
+        handler: (request, h) => {
+            return getAllCartItems();
+        }
+    });
+};
+
+const getCartItemsByItemIdRoute = (server) => {
+    server.route({
+        path: '/cart-items/{cartItemId}',
+        method: 'GET',
+        handler: (request, h) => {
+            const cartItems = getCartItemsByItemId(request.params.cartItemId)
+
+            if (!cartItems) {
+                return h.response().code(404);
+            }
+
+            return cartItems;
+        }
+    });
+};
+
+const getCartItemsByCartItemIdRoute = (server) => {
+    server.route({
+        path: '/cart-items/{cartItemId}',
+        method: 'GET',
+        handler: (request, h) => {
+            const cartItemId = getCartItemsByCartItemId(request.params.cartItemId);
+
+            if (!cartItemId) {
+                return h.response().code(404);
+            }
+            
+            return cartItemId;
+        }
+    });
+};
+
+const initCartItemControllers = (server) => {
+    getCartItemsRoute(server);
+    getCartItemsByItemIdRoute(server);
+    getCartItemsByCartItemIdRoute(server);
+};
+
+module.exports = {
+    initCartItemControllers
+};
