023ab335eab3bedf5ad544c2226e1df2ffc1b6fe
SamuelDowney <43819573+SamuelDowney@users.noreply.github.com>
2020-04-26 08:07:41 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,36 +1,30 @@
-const item = {
-    itemId: '02bbdbc7-e22e-4153-abd8-b5732a4ba6b5',
-    name: 'Frying Pan',
-    description: 'Totally awesome frying pan made of cast iron!',
-    price: 999.99,
-    sizesAvailable: 'XXL, XL, L, M, S, XS'
+import Link from 'next/link';
+import fetch from 'isomorphic-unfetch';
+
+const Index = props => (
+  <section>
+    <h1>Items Available for Sale</h1>
+    <ul>
+      {props.items.map(item => (
+        <li key={item.itemId}>
+          <Link href="/items/[itemId]" as={`/items/${item.itemId}`}>
+            <a>{item.description}</a>
+          </Link>
+        </li>
+      ))}
+    </ul>
+  </section>
+);
+
+Index.getInitialProps = async function() {
+  const res = await fetch('http://localhost:5555/items');
+  const items = await res.json();
+
+  console.log(`Show data fetched. Count: ${items.length}`);
+
+  return {
+    items
+  };
 };
 
-const firstName = 'Samuel';
-const lastName = 'Downy';
-
-const customer = {
-    customerId: 'qow9me28-1jq9-43b8-2e0i-536m83w31eq3',
-    firstName,
-    lastName,
-    email: `${firstName.toLowerCase()}.${lastName.toLowerCase()}@drake.edu`,
-    phoneNumber: '+15155555555'
-};
-
-const cart = {
-	cartId: 'aiw8ja98-1wq8-52b9-7m0q-539j90m48oe2',
-	customerId: 'qow9me28-1jq9-43b8-2e0i-536m83w31eq3',
-	createdDate: '2020-02-02',
-	purchasedDate: '2025-02-02'
-};
-
-const cartItem = {
-	itemId: '02bbdbc7-e22e-4153-abd8-b5732a4ba6b5',
-	cartId: 'aiw8ja98-1wq8-52b9-7m0q-539j90m48oe2',
-	quantity: 5
-};
-
-console.log('item', item);
-console.log('customer', customer);
-console.log('cart', cart);
-console.log('cartItem', cartItem);
+export default Index; 
